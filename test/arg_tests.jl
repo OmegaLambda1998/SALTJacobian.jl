8f4f7889d29ddabea2b19dc59d321fa0bc2de9fe
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2023-05-16 21:34:15 +1000

    Fixing tests

--- test/arg_tests.jl
+++ test/arg_tests.jl
@@ -10,35 +10,35 @@
 
             # Missing --output
             @test_throws ArgumentError main(args)
-            @test_throws "Must specify an output directory" main(args)
+            @test_throws ArgumentError("Must specify an output directory via --output/-o when in batch mode") main(args)
             push!(args, "--output")
             push!(args, "Outputs/arg_tests/batch_mode_invalid/")
-            
+
             # Missing --jacobian
             @test_throws ArgumentError main(args)
-            @test_throws "Must specify a pretrained jacobian matrix" main(args)
+            @test_throws ArgumentError("Must specify a pretrained jacobian matrix via --jacobian/-j when in batch mode") main(args)
             push!(args, "--jacobian")
             push!(args, "does_not_exist.fits")
 
             # --jacobian does not exist
             @test_throws ArgumentError main(args)
-            @test_throws "does not exist" main(args)
+            @test_throws ArgumentError("Pretrained jacobian matrix does_not_exist.fits does not exist") main(args)
             args[end] = "Inputs/arg_tests/batch_mode_invalid_jacobian.fits"
 
             # Missing --base
             @test_throws ArgumentError main(args)
-            @test_throws "Must specify a base (unperturbed) surface" main(args)
+            @test_throws ArgumentError("Must specify a base (unperturbed) surface via --base/-b when in batch mode") main(args)
             push!(args, "--base")
             push!(args, "does_not_exist")
 
             # --base does not exist
             @test_throws ArgumentError main(args)
-            @test_throws "does not exist" main(args)
+            @test_throws ArgumentError("Base surface does_not_exist does not exist") main(args)
             args[end] = "Inputs/arg_tests/batch_mode_invalid_base_surface/"
 
             # Missing --trainopt
             @test_throws ArgumentError main(args)
-            @test_throws "Must specify trainopt" main(args)
+            @test_throws ArgumentError("Must specify trainopt via --trainopt/-t when in batch mode") main(args)
             push!(args, "--trainopt")
             args = vcat(args, "\"MAGSHIFT Instrument Filter 0.01\"")
         end
@@ -48,7 +48,7 @@
         @testset "Invalid Arguments" begin
             args = String[]
             @test_throws ArgumentError main(args)
-            @test_throws "must specify an input file" main(args)
+            @test_throws ArgumentError("If not working in batch mode, must specify an input file!") main(args)
         end
     end
 end
